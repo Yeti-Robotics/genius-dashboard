aa090fcc5f07355ec653b2a88f27587a999c42bd
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2023-03-25 14:35:14 -0400

    add improved release script using deno 🦕

---
+++ release.ts
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write
+import {
+	increment,
+	ReleaseType,
+} from 'https://deno.land/std@0.181.0/semver/mod.ts';
+
+type TauriConfig = {
+	package: {
+		version: string;
+	};
+};
+
+const config: TauriConfig = JSON.parse(
+	Deno.readTextFileSync('./src-tauri/tauri.conf.json')
+);
+
+const newVer = increment(config.package.version, Deno.args[0] as ReleaseType);
+
+if (newVer === null) {
+	console.error('Somethings went wrong updating the version, args:', Deno.args);
+	Deno.exit(1);
+}
+
+config.package.version = newVer;
+Deno.writeTextFileSync(
+	'./src-tauri/tauri.conf.json',
+	JSON.stringify(config, null, '\t') + '\n'
+);
+
+const run = (args: string[]) => {
+	const process = Deno.run({ cmd: args, stdout: 'piped' });
+	return Promise.all([process.output(), process.status()]).then(() =>
+		process.close()
+	);
+};
+
+/*
+git add .
+git commit -m "[release] $1"
+git push origin
+git tag $1
+git push origin $1
+*/
+
+await run(['git', 'add', '.']);
+await run(['git', 'commit', '-m', `"[release] ${newVer}"`]);
+await run(['git', 'push', 'origin']);
+await run(['git', 'tag', newVer]);
+await run(['git', 'push', 'origin', newVer]);
+
+
